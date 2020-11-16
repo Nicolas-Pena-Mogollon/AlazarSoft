b6879ee32e812ed1b582f3c3fb8241091b125ee7
Nicolas-Pena-Mogollon <71480273+Nicolas-Pena-Mogollon@users.noreply.github.com>
2020-11-16 18:49:15 -0500

    Cambio interfaz

--- AlazarSoft/src/co/edu/unbosque/controller/AplMain.java
+++ AlazarSoft/src/co/edu/unbosque/controller/AplMain.java
@@ -3,15 +3,13 @@ package co.edu.unbosque.controller;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import com.jtattoo.plaf.noire.NoireLookAndFeel;
-
 public class AplMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		try {
-			UIManager.setLookAndFeel(NoireLookAndFeel.class.getName());
+			UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
 
 		} catch (UnsupportedLookAndFeelException e) {
 		} catch (ClassNotFoundException e) {
