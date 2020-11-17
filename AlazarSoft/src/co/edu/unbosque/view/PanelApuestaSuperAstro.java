940243278104a5ef45b269fb59b16236024a53a0
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2020-11-16 19:06:51 -0500

    creacion Jcombobox

--- AlazarSoft/src/co/edu/unbosque/view/PanelApuestaSuperAstro.java
+++ AlazarSoft/src/co/edu/unbosque/view/PanelApuestaSuperAstro.java
@@ -3,30 +3,35 @@ package co.edu.unbosque.view;
 import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
-public class PanelApuestaSuperAstro extends JPanel {
+public class PanelApuestaSuperAstro extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	private JLabel etiquetaNumero;
+	private JLabel etiquetaSigno;
 	private JTextField campoTextoPrimerNumero;
 	private JTextField campoTextoSegundoNumero;
 	private JTextField campoTextoTercerNumero;
 	private JTextField campoTextoCuartoNumero;
+	private JComboBox<String> SignoZodiacal;
 	private JPanel panelCamposDeTextoNumero;
 
 	public PanelApuestaSuperAstro() {
 		setBorder(new TitledBorder("Apuesta baloto"));
 		setLayout(new GridLayout(1, 2));
 
+		SignoZodiacal = new JComboBox<>();
+		etiquetaSigno = new JLabel("Signo del Zodiaco");
 		etiquetaNumero = new JLabel("Número");
 
 		panelCamposDeTextoNumero = new JPanel();
-		panelCamposDeTextoNumero.setLayout(new GridLayout(1, 4));
+		panelCamposDeTextoNumero.setLayout(new GridLayout(2, 5));
 
 		campoTextoPrimerNumero = new JTextField();
 		campoTextoSegundoNumero = new JTextField();
@@ -45,6 +50,25 @@ public class PanelApuestaSuperAstro extends JPanel {
 
 		this.add(etiquetaNumero);
 		this.add(panelCamposDeTextoNumero);
+		this.add(etiquetaSigno);
+		this.add(SignoZodiacal);
+	}
+	
+	public void llenarComboZodiaco() {
+		SignoZodiacal.addItem("Seleccione un signo");
+		SignoZodiacal.addItem("Aries");
+		SignoZodiacal.addItem("Tauro");
+		SignoZodiacal.addItem("Geminis");
+		SignoZodiacal.addItem("Cancer");
+		SignoZodiacal.addItem("Leo");
+		SignoZodiacal.addItem("Virgo");
+		SignoZodiacal.addItem("Libra");
+		SignoZodiacal.addItem("Escorpio");
+		SignoZodiacal.addItem("Sagitario");
+		SignoZodiacal.addItem("Capricornio");
+		SignoZodiacal.addItem("Acuario");
+		SignoZodiacal.addItem("Piscis");
+
 	}
 
 	public JTextField getCampoTextoPrimerNumero() {
@@ -77,6 +101,30 @@ public class PanelApuestaSuperAstro extends JPanel {
 
 	public void setCampoTextoCuartoNumero(JTextField campoTextoCuartoNumero) {
 		this.campoTextoCuartoNumero = campoTextoCuartoNumero;
+	}
+
+	public JLabel getEtiquetaNumero() {
+		return etiquetaNumero;
+	}
+
+	public void setEtiquetaNumero(JLabel etiquetaNumero) {
+		this.etiquetaNumero = etiquetaNumero;
+	}
+
+	public JComboBox<String> getSignoZodiacal() {
+		return SignoZodiacal;
+	}
+
+	public void setSignoZodiacal(JComboBox<String> signoZodiacal) {
+		SignoZodiacal = signoZodiacal;
+	}
+
+	public JPanel getPanelCamposDeTextoNumero() {
+		return panelCamposDeTextoNumero;
+	}
+
+	public void setPanelCamposDeTextoNumero(JPanel panelCamposDeTextoNumero) {
+		this.panelCamposDeTextoNumero = panelCamposDeTextoNumero;
 	}
 
 }
