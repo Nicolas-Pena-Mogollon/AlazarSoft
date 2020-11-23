daca2b41d21e788f97677446f266e5a1b7425895
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2020-11-22 21:19:10 -0500

    JUnit clases

--- AlazarSoft/test/src/co/edu/unbosque/test/BalotoDaoTest.java
+++ AlazarSoft/test/src/co/edu/unbosque/test/BalotoDaoTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CasaDeApuestasTest {
+public class BalotoDaoTest {
 	@Before
 	public void setUp() throws Exception {
 
