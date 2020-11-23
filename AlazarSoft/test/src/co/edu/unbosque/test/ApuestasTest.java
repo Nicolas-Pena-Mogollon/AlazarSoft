aca8d4971bc293c91cb67776aa609bab8b86d2c6
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2020-11-23 11:30:22 -0500

    Arreglo de JUnit Ya no se escribe en los archivos pero tengan cuidado porque el codigo se puede explotar (Ya me exploto eclipse)

--- AlazarSoft/test/src/co/edu/unbosque/test/ApuestasTest.java
+++ AlazarSoft/test/src/co/edu/unbosque/test/ApuestasTest.java
@@ -7,16 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import co.edu.unbosque.model.Apuesta;
-import co.edu.unbosque.model.persistence.BalotoDTO;
-import co.edu.unbosque.model.persistence.MarcadoresDTO;
-import co.edu.unbosque.model.persistence.SuperastroDTO;
 
 public class ApuestasTest {
 
 	Apuesta apuestas;
-	BalotoDTO baloto1;
-	MarcadoresDTO marcadores1;
-	SuperastroDTO superastro1;
 
 	@Before
 	public void setUp() throws Exception {
