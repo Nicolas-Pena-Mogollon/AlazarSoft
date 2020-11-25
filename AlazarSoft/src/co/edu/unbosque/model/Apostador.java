09831098ca3ab08168178e099f2ded78584267e1
Robinson <Robinson@DESKTOP-1ERL727>
2020-11-24 21:53:12 -0500

    Listo cedula

--- AlazarSoft/src/co/edu/unbosque/model/Apostador.java
+++ AlazarSoft/src/co/edu/unbosque/model/Apostador.java
@@ -71,7 +71,7 @@ public class Apostador {
 	public void verificarCelular(String celular) throws CelularException {
 		if (!soloNumeros(celular)) {
 			throw new CelularException("Ingresó un caracter no permitido para el teléfono");
-		} else if ((celular.length() <= 10 || celular.length() > 5) && celular.charAt(0) == '3') {
+		} else if (celular.length() == 10 && celular.charAt(0) == '3') {
 		} else {
 			throw new CelularException("Número telefónico incorrecto");
 		}
@@ -89,7 +89,7 @@ public class Apostador {
 	public void verificarCedula(String cedula) throws CedulaException {
 		if (!soloNumeros(cedula)) {
 			throw new CedulaException("Ingresó un caracter no permitido para la cedula");
-		} else if (cedula.length() == 10) {
+		} else if (cedula.length() <= 10 && cedula.length() > 5 ) {
 		} else {
 			throw new CedulaException("Cedula incorrecta");
 		}
